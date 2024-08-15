f50f79a2b0eecfc7567dad254ed89c06c7464e7b
Asz <126867549+DadaLober@users.noreply.github.com>
2024-08-15 18:58:34 +0800

    add errors

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -9,7 +9,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             const response = await axios.post('http://localhost:8080/login', body);
             res.status(200).json(response.data);
         } catch (error) {
-            res.status(401).json({ message: 'Password or email is incorrect' });
+            console.error('Error submitting form:', error);
+            res.status(500).json({ message: 'Error submitting form' });
         }
     } else {
         res.status(405).json({ message: 'Method not allowed' });
